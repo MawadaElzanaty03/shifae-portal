d41153ee76771e73413c6979e30fc33744a5c9c8
mawadaalzanaty <mawda.abdo2003@gmail.com>
2026-05-13 22:05:45 +0200

    Update User.php

--- shifae-portal/app/Models/User.php
+++ shifae-portal/app/Models/User.php
@@ -19,9 +19,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'userName',    
-        'name',
+        'fullName',
         'password',
-        'role',
+        'userRole',
     ];
 
     /**
