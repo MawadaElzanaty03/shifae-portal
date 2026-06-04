c29cbdb8890e7be6dec60db9c0e3a1c53d79bab6
mawadaalzanaty <mawda.abdo2003@gmail.com>
2026-06-04 18:54:33 +0200

    اضافة state design patreen  و جدول الخاص بعمليات الدفع

---
+++ shifae-portal/app/States/PendingState.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\States;
+
+use App\Models\Booking;
+use Exception;
+
+class PendingState implements BookingStateInterface
+{
+    public function confirmAttendanceAndPay(Booking $booking, $amount, $paymentMethod)
+    {
+        // بما أن الموعد معلق، نسمح له بالدفع والتحديث
+        $booking->update([
+            'status' => 'completed/paid',
+            'amount_paid' => $amount,
+            'payment_method' => $paymentMethod
+        ]);
+
+        return true;
+    }
+}
