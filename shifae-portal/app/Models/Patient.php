0b9b1d3d2627df5853e81c501b646bca7004c9c7
asraalmsrwb-afk <asraalmsrwb@gmail.com>
2026-06-17 12:30:58 +0200

    اضافة الدوال الاحصائية السنوية( الجنس و الفئات العمرية) داخل نماذج المريض و الحجوزات

--- shifae-portal/app/Models/Patient.php
+++ shifae-portal/app/Models/Patient.php
@@ -23,4 +23,20 @@ class Patient extends Model
     public function records() {
         return $this->hasMany(Record::class, 'patientId');
     }
+    public static function getGenderStatistics($year){//دالة مخصصة لجلب عدد الذكور و الاناث
+    return [
+        'maleCount' => self::where('gender', 'male')->whereYear('created_at',$year)->count(),
+        'femaleCount' => self::where('gender', 'female')->whereYear('created_at',$year)->count(),
+    ];
 }
+   public static function getAgeStatistics($year)
+{
+//دالة مخصصه لجلب عدد الاطفال و الكبار
+    $eighteenYearsAgo = \Carbon\Carbon::now()->subYears(15);//يعتبر المريض طفل اذا كان تحت ال 15
+    return [
+        'childrenCount' => self::whereDate('dateOfBirth', '>', $eighteenYearsAgo)->whereYear('created_at',$year)->count(),
+        'adultsCount' => self::whereDate('dateOfBirth', '<=', $eighteenYearsAgo)->whereYear('created_at',$year)->count(),
+    ];
+}
+}
+
